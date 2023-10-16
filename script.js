c1353d53ede890b0e9f08f4395a71dbde9fde820
Koch <93111779+Koch-Dev@users.noreply.github.com>
2023-10-16 21:46:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+// Existing code for the slideshow
 let slideIndex = 0;
 const slides = document.getElementsByClassName("mySlides");
 
@@ -11,10 +11,30 @@ function showSlides() {
 
 showSlides();
 
+// Load and display blog content when the "Blog" link is clicked
 const blogLink = document.getElementById("blog-link");
 const blogContent = document.getElementById("blog-content");
 
-blogLink.addEventListener("click", function(event) {
+blogLink.addEventListener("click", async function (event) {
     event.preventDefault();
-    blogContent.style.display = "block";
+
+    // Update this array with the names of your blog HTML files
+    const blogFileNames = ["blog1.html", "blog2.html", "blog3.html"];
+
+    // Randomly select a blog to display (you can modify this logic)
+    const randomBlogIndex = Math.floor(Math.random() * blogFileNames.length);
+    const selectedBlogFileName = blogFileNames[randomBlogIndex];
+
+    // Load and display the selected blog content
+    try {
+        const response = await fetch(`blogs/${selectedBlogFileName}`);
+        if (response.ok) {
+            const blogHTML = await response.text();
+            blogContent.innerHTML = blogHTML;
+        } else {
+            console.error('Failed to load the blog content.');
+        }
+    } catch (error) {
+        console.error('An error occurred while loading the blog content:', error);
+    }
 });
